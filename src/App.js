86ef1ab89fbfb60e0d70530bc6f483ad62becf73
julianv97 <julian.vicente97s@gmail.com>
2021-08-12 14:07:59 -0300

    handleMaximize

--- src/App.js
+++ src/App.js
@@ -7,11 +7,26 @@ import ToolBar from "./components/ToolBar";
 
 function App() {
   const [markdown, setMarkdown] = useState(data);
+  const [isHiddenEditor, setIsHiddenEditor] = useState(false)
+  const [isHiddenPreviewer, setIsHiddenPreviewer] = useState(false)
+
+  const handleMaxiEditor = () => {
+    setIsHiddenPreviewer(!isHiddenPreviewer)
+    
+    console.log('entra')
+  }
+
+  const handleMaxiPreviewer = () => {
+    setIsHiddenEditor(!isHiddenEditor)
+    
+    console.log('entra2')
+  }
+
   return (
     <main className="bg-grayish-green">
       <section className=" w-full flex flex-col  justify-center items-center pt-10 ">
-        <div className="w-5/12 border-2 border-gray-800 shadow-2xl mb-10">
-          <ToolBar title={"editor"} />
+        {!isHiddenEditor && <div className="w-5/12 border-2 border-gray-800 shadow-2xl mb-10">
+          <ToolBar title={"editor"} handleMaxi={handleMaxiEditor} />
           <article className="w-full flex justify-center   ">
             <textarea
               name="textarea"
@@ -22,16 +37,19 @@ function App() {
               className="w-full overflow-y-auto input text-md font-radjani text-gray-900  bg-desaturated-blue px-2 pt-2 outline-none"
             ></textarea>
           </article>
-        </div>
+        </div>}
+        
 
-        <div className="w-8/12 border-2 border-gray-800 shadow-2xl mb-10">
-          <ToolBar title={"previewer"} />
+        {!isHiddenPreviewer && <div className="w-8/12 border-2 border-gray-800 shadow-2xl mb-10">
+          <ToolBar title={"previewer"} handleMaxi={handleMaxiPreviewer} />
           <article id="preview" className="w-full bg-desaturated-blue h-full ">
             <ReactMarkdown remarkPlugins={[gfm]} className="px-6">
               {markdown}
             </ReactMarkdown>
           </article>
-        </div>
+        </div>}
+
+        
       </section>
     </main>
   );
--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { FaExpandArrowsAlt } from 'react-icons/fa';
-import { GrContract } from 'react-icons/gr';
+import React from "react";
+import { FaExpandArrowsAlt } from "react-icons/fa";
+import { GrContract } from "react-icons/gr";
 
-function ToolBar({title}) {
-    return (
-        <div className="flex justify-between">
-            <h3>{title}</h3>
-
-            <div>
-            <FaExpandArrowsAlt />
-            <GrContract />
-            </div>
-        </div>
-    )
+function ToolBar({ title, handleMaxi }) {
+  return (
+    <div className="flex justify-between">
+      <h3>{title}</h3>
+      <div>
+        <FaExpandArrowsAlt onClick={handleMaxi}  className="cursor-pointer"/>
+        <GrContract />
+      </div>
+    </div>
+  );
 }
 
-export default ToolBar
+export default ToolBar;
